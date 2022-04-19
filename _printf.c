cb20b986bed0472c19b4ef0a9d752acd388802f8
Olwanishola <lukusope@yahoo.com>
2022-04-19 07:34:33 -0700

    _printf.c

--- _printf.c
+++ _printf.c
@@ -1,48 +1,47 @@
-#include <stdio.h>
-#include <stdarg.h>
 #include "main.h"
+
 /**
- * _printf - prints anything
- * @format: pointer to string that contains specifiers
- * Return: number of characters printed
- **/
+ * _printf - produces output according to a format
+ * @format: format string containing the characters and the specifiers
+ * Description: this function will call the get_print() function that will
+ * determine which printing function to call depending on the conversion
+ * specifiers contained into fmt
+ * Return: length of the formatted output string
+ */
 int _printf(const char *format, ...)
 {
-	unsigned int count = 0, i = 0;
-	int (*f)(va_list);
-	va_list list;
+	int (*pfunc)(va_list, flags_t *);
+	const char *p;
+	va_list arguments;
+	flags_t flags = {0, 0, 0};
 
-	if (format == '\0')
+	register int count = 0;
+
+	va_start(arguments, format);
+	if (!format || (format[0] == '%' && !format[1]))
 		return (-1);
-	va_start(list, format);
-	while (format && format[i])
+	if (format[0] == '%' && format[1] == ' ' && !format[2])
+		return (-1);
+	for (p = format; *p; p++)
 	{
-		if (format[i] != '%')
+		if (*p == '%')
 		{
-			_putchar(format[i]);
-			count++;
-		}
-		else if (format[i] == '%' && format[i + 1] == '\0')
-			return (-1);
-		else if (format[i] == '\0')
-			return (count);
-		else if (format[i] == '%')
-		{
-			f = getspecifier(format, i + 1);
-			i += 1;
-			if (f == NULL)
+			p++;
+			if (*p == '%')
 			{
-				count += strange(format, i);
+				count += _putchar('%');
+				continue;
 			}
-			else
-			{
-				count = count + f(list);
-				if (format[i] == '+' || format[i] == ' ' || format[i] == '#')
-					i++;
-			}
-		}
-		i++;
+			while (get_flag(*p, &flags))
+				p++;
+			pfunc = get_print(*p);
+			count += (pfunc)
+				? pfunc(arguments, &flags)
+				: _printf("%%%c", *p);
+		} else
+			count += _putchar(*p);
 	}
-	va_end(list);
+	_putchar(-1);
+	va_end(arguments);
 	return (count);
 }
